a7bcef265d1ff69f5ab6e8048773d24079b474bc
Andrej Belica <belicadud0@gmail.com>
2026-08-16 20:48:55 +0200

    updated version

--- Formula/terminal-groove.rb
+++ Formula/terminal-groove.rb
@@ -1,8 +1,8 @@
 class TerminalGroove < Formula
   desc "Terminal groovebox for creating music in the terminal"
   homepage "https://github.com/dud0/terminal-groove"
-  url "https://github.com/dud0/terminal-groove/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "6a457bf8315a8c81bc4f2aec06facb8f32dddf4beabf11b4f255954577036fcc"
+  url "https://github.com/dud0/terminal-groove/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "d8f821b49f6637cbb9e84d4b10117ba294f15b02a41cea921a37872f7c523b27"
   license "MIT"
 
   depends_on "rust" => :build
